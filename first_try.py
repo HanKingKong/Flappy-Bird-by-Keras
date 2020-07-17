72ca050973ba7e56826ed705ad9a4e58a2735087
HanKingKong <1328968413@qq.com>
2020-07-17 12:32:58 +0800

    first commit

---
+++ first_try.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("game/")
+import cv2
+import wrapped_flappy_bird as game
+
+game_state = game.GameState()
+
+do = [0, 1]
+
+image, reward, terminal = game_state.frame_step(do)
+
+
